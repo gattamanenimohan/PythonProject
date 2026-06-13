d988b13230008edaf11cdf7f7bf9cfeeed5cefd0
gattamaneni mohan <gattamanenimohan32gmail.com>
2026-06-13 22:21:45 +0530

    added

--- Inteview/gattamaneni.py
+++ Inteview/gattamaneni.py
@@ -19,3 +19,16 @@ lsit = [10, 20, 30, 20, 20, "mohan", "krishna", "mohan"]
 
 duplicates = [i for i, count in Counter(lsit).items() if count >1]
 print("Duplicated values:", duplicates)
+
+# weapin teh upper and lower case
+
+text = "My  self  is   mohan"
+text = text.replace("m", "M", 1).replace("M", "m", 1)
+
+# count → the maximum number of occurrences to replace, starting from the left.
+# print(text.replace("m", "M", 1))  # replaces first 'm'
+# print(text.replace("m", "M", 2))  # replaces first two 'm'
+# print(text.replace("m", "M", 3))  # replaces first three 'm'
+print(text)
+
+
