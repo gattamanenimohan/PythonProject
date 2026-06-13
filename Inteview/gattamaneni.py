a1f3bde35e59b490f1c03e18740485e40a29dd6f
gattamaneni mohan <gattamanenimohan32gmail.com>
2026-06-13 21:50:19 +0530

    updatinglist of duplicates

--- Inteview/gattamaneni.py
+++ Inteview/gattamaneni.py
@@ -1,4 +1,16 @@
 list= [10,20,30,20,20]
 
 print(list)
+print()
 print(type(list))
+
+from collections import Counter
+
+lst = [10, 20, 30, 20, 20]
+duplicates = [item for item, count in Counter(lst).items() if count > 1]
+print("Duplicated values:", duplicates)
+
+tup = (10, 20, 30, 20, 20)
+duplicates = [item for item, count in Counter(tup).items() if count > 1]
+print("Duplicated values:", duplicates)
+
